b64987c7493af1e7086c7d76c93e00a24eb1b7ff
Rajasree Laha <laharajasree121@gmail.com>
2024-04-02 10:30:13 +0530

    Create addition _without _using _scanner.java

---
+++ addition _without _using _scanner.java
@@ -0,0 +1,11 @@
+/*addition without using scanner*/
+class add_without_scanner_include_public
+{
+ public static void sum(int x,int y )
+ {
+ int z;
+ z=x+y;
+ System.out.println("addition is");
+ System.out.println(z);
+ }
+}
