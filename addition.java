a9abcd5861e63fa88039f305021f8862d44c3a3c
Rajasree Laha <laharajasree121@gmail.com>
2024-04-02 10:37:07 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,17 @@
+/*addition*/
+import java.util.Scanner;
+public class add
+{
+ public static void main(String args[])
+ {
+ int x,y,z;
+ System.out.println("enter 2 no");
+ Scanner sc=new Scanner(System.in);
+ x=sc.nextInt();
+ y=sc.nextInt();
+ z=x+y;
+ System.out.println("addition is");
+ System.out.println(z);
+ 
+ }
+}
