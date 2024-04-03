18e59adaa0895eb77a7499885cfd7a3fb400a6ee
Rajasree Laha <laharajasree121@gmail.com>
2024-04-03 10:56:33 +0530

    Create alphabet pyramid.java

---
+++ alphabet pyramid.java
@@ -0,0 +1,24 @@
+/* alphabet pyramid */
+import java.util.*;
+public class alphabet_pyramid
+{
+ public static void main(String args[])
+ {
+ int n,i,j,k,c=65;
+ System.out.println("enter the value of n");
+ Scanner sc=new Scanner(System.in);
+ n=sc.nextInt();
+ for(i=0;i<n;i++)
+ {
+ for(j=0;j<n-i-1;j++)
+ {
+ System.out.print(" ");
+ }
+ for(k=0;k<2*i+1;k++)
+ {
+ System.out.print((char)(k+c));
+ }
+ System.out.println("");
+ }
+ }
+}
