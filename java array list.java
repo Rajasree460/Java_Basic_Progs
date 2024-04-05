e10738dda40ed9c8a75c883204667182b902849c
Rajasree Laha <laharajasree121@gmail.com>
2024-04-05 12:11:39 +0530

    Create java array list.java

---
+++ java array list.java
@@ -0,0 +1,13 @@
+/*java array list*/
+import java.util.ArrayList;
+//import java.util.*;
+public class Print_arraylist { 
+ public static void main(String[] args) { 
+ ArrayList<String> cars = new ArrayList<String>(); //obj declaration
+ cars.add("Volvo");
+ cars.add("BMW");
+ cars.add("Ford");
+ cars.add("Mazda");
+ System.out.println(cars);
+ } 
+}
