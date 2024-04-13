7e329fad2cb8dddc66e9835e895b63a404219272
Rajasree Laha <laharajasree121@gmail.com>
2024-04-13 10:11:56 +0530

    Create Java Program to count the total number of characters in a  string.java

---
+++ Java Program to count the total number of characters in a  string.java
@@ -0,0 +1,20 @@
+/*Java Program to count the total number of characters in a 
+string*/
+public class CountCharacter_string 
+{ 
+public static void main(String[] args)
+{ 
+String string = "rajasree"; 
+int count = 0; 
+//Counts each character except space 
+for(int i = 0; i < string.length(); i++) 
+{ 
+if(string.charAt(i) != ' ') 
+{
+count++; 
+} 
+}
+//Displays the total number of characters present in the given string 
+System.out.println("Total number of characters in a string: " + count); 
+} 
+}
