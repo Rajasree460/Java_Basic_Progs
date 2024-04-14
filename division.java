35a61d844c8d2dba63b54b15eeaf74cb07ef5135
Rajasree Laha <laharajasree121@gmail.com>
2024-04-14 12:39:24 +0530

    Create division.java

---
+++ division.java
@@ -0,0 +1,17 @@
+/*division*/
+import java.util.Scanner;
+public class division
+{
+ public static void main(String args[])
+ {
+ int x,y,z;
+ System.out.println("enter 2 no");
+ Scanner sc=new Scanner(System.in);
+ x=sc.nextInt();
+ y=sc.nextInt();
+ z=x/y;
+ System.out.println("division is");
+ System.out.println(z);
+ 
+ }
+}
