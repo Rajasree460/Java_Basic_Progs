ec0d38f32233f6107b313c8aad2d24b174e52d54
Rajasree Laha <laharajasree121@gmail.com>
2024-05-12 10:06:52 +0530

    Create math function al I.java

---
+++ math function al I.java
@@ -0,0 +1,57 @@
+/*math function all*/
+public class math_func_t
+{
+ public static void main(String args[])
+ {
+ double k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16,k17,k18,k19;
+ k1=Math.log(1);
+ k2=Math.sin(90);
+ k3=Math.cos(0);
+ k4=Math.tan(45);
+ k5=(1/k2);
+ k6=(1/k3);
+ k7=(1/k4);
+ k8=Math.random();
+ k9=3.56;
+ k14=-5;
+ k16=30;
+ k17=60;
+ System.out.println("value of log(1) is");
+ System.out.println(k1);
+ System.out.println("value of sin(90) is");
+ System.out.println(k2);
+ System.out.println("value of cos(0) is");
+ System.out.println(k3);
+ System.out.println("value of tan(45) is");
+ System.out.println(k4);
+ System.out.println("value of cosec(90)/k5 is");
+ System.out.println(k5);
+ System.out.println("value of sec(0)/k6 is");
+ System.out.println(k6);
+ System.out.println("value of cot(45)/k7 is");
+ System.out.println(k7);
+ System.out.println("a random no is");
+ System.out.println(k8);
+ k10=Math.floor(k9);
+ System.out.println("the floor value of 3.56 is:");
+ System.out.println(k10);
+ k11=Math.ceil(k9);
+ System.out.println("the celling value of 3.56 is:");
+ System.out.println(k11);
+ k12=Math.rint(k9);
+ System.out.println("the rint(round off int)value of 3.56 is:");
+ System.out.println(k12);
+ k13=Math.abs(k14);
+ System.out.println("the abs value of -5 is:");
+ System.out.println(k13);
+ k15=Math.round(k9);
+ System.out.println("the round off value of 3.56 is:");
+ System.out.println(k15);
+ k18=Math.atan2(k16,k17);
+ System.out.println("the exp value of 3.56 is:");
+ System.out.println(k18);
+ k19=Math.tanh(k17);
+ System.out.println("the tanh value of 60 is:");
+ System.out.println(k19);
+ }
+}
