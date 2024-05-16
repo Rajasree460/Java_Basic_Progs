5dbe8730e42661a56c1128e616392e1df79b0813
Rajasree Laha <laharajasree121@gmail.com>
2024-05-16 14:29:36 +0530

    Create math function trigonametry.java

---
+++ math function trigonametry.java
@@ -0,0 +1,32 @@
+/*math function trigonametry*/
+public class math_func_t
+{
+ public static void main(String args[])
+ {
+ double k1,k2,k3,k4,k5,k6,k7,k8;
+ k1=Math.log(1);
+ k2=Math.sin(90);
+ k3=Math.cos(0);
+ k4=Math.tan(45);
+ k5=(1/k2);
+ k6=(1/k3);
+ k7=(1/k4);
+ k8=Math.random();
+ System.out.println("value of log(1) is");
+ System.out.println(k1);
+ System.out.println("value of sin(90) is");
+ System.out.println(k2);
+ System.out.println("value of cos(0) is");
+ System.out.println(k3);
+ System.out.println("value of tan(45) is");
+ System.out.println(k4);
+ System.out.println("value of cosec(90)/k5 is");
+ System.out.println(k5);
+ System.out.println("value of sec(0)/k6 is");
+ System.out.println(k6);
+ System.out.println("value of cot(45)/k7 is");
+ System.out.println(k7);
+ System.out.println("a random no is");
+ System.out.println(k8);
+ }
+}
