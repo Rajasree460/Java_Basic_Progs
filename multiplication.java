6e60ef97ec276683d98ec810b971a9c4ffdd1153
Rajasree Laha <laharajasree121@gmail.com>
2024-05-27 10:12:36 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,17 @@
+/*multiplication*/
+import java.util.Scanner;
+public class multiplication
+{
+ public static void main(String args[])
+ {
+ int x,y,z;
+ System.out.println("enter 2 no");
+ Scanner sc=new Scanner(System.in);
+ x=sc.nextInt();
+ y=sc.nextInt();
+ z=x*y;
+ System.out.println("multiplication is");
+ System.out.println(z);
+ 
+ }
+}
