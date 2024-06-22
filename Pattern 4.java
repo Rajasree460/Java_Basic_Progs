27639f45d878320123fc814e8bb49dd21ef0e9a5
Rajasree Laha <laharajasree121@gmail.com>
2024-06-22 14:09:52 +0530

    Create Pattern 4.java

---
+++ Pattern 4.java
@@ -0,0 +1,22 @@
+//Pattern 4
+/*2 
+ 24 
+ 246 
+ 2468 */ 
+public class pattern_4
+{
+ public static void main(String args[])
+ {
+ int i,j;
+ for(i=1;i<5;i++)
+ {
+ for(j=1;j<=i;j++)
+ {
+ 
+ System.out.print(j*2);
+ 
+ }
+ System.out.println(" ");
+ }
+ }
+}
