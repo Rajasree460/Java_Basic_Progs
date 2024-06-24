cee6b3e5527c78d9e31d8b1d7bf62e090e658b72
Rajasree Laha <laharajasree121@gmail.com>
2024-06-24 09:11:15 +0530

    Create Pattern 6.java

---
+++ Pattern 6.java
@@ -0,0 +1,23 @@
+//Pattern 6
+/* 5 5 5 5 5 
+ 4 4 4 4 
+ 3 3 3 
+ 2 2 
+ 1 */ 
+public class pattern_6
+{
+ public static void main(String args[])
+ {
+ int i,j;
+ for(i=5;i>0;i--)
+ {
+ for(j=0;j<i;j++)
+ {
+ 
+ System.out.print(i+ " ");
+ 
+ }
+ System.out.println(" ");
+ }
+ }
+}
