6e14476c64dcde0c6db60e3b30ab0bc08c15b4c4
Rajasree Laha <laharajasree121@gmail.com>
2024-06-29 12:17:46 +0530

    Create Pattern--_.java

---
+++ Pattern--_.java
@@ -0,0 +1,23 @@
+//Pattern--_
+/*1 
+ 2 2 
+ 3 3 3 
+ 4 4 4 4 
+ 5 5 5 5 5*/ 
+public class break_55
+{
+ public static void main(String args[])
+ {
+ int i,j;
+ for(i=1;i<=5;i++)
+ {
+ for(j=0;j<i;j++)
+ {
+ 
+ System.out.print(i+ " ");
+ 
+ }
+ System.out.println(" ");
+ }
+ }
+}
