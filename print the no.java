180e06117a7064a47d9b855b56b48393a16a49b5
Rajasree Laha <laharajasree121@gmail.com>
2024-07-07 09:54:49 +0530

    Create print the no.java

---
+++ print the no.java
@@ -0,0 +1,15 @@
+/*print the no.*/
+import java.util.*;
+public class print_no
+{
+ public static void main(String []args)
+ {
+ System.out.println("enter a value");
+ Scanner sc=new Scanner(System.in);
+ int n=sc.nextInt();
+ for(int i=0;i<n;i++)
+ {
+ System.out.printf(i+" ");
+ }
+ }
+}
