69be88ad5e4b5fad4d9c9a7815aba6a92488a61e
Rajasree Laha <laharajasree121@gmail.com>
2024-07-08 10:43:27 +0530

    Create print the size of arraylist.java

---
+++ print the size of arraylist.java
@@ -0,0 +1,12 @@
+/*print the size of arraylist*/
+import java.util.ArrayList;
+public class Arraylist_size { 
+public static void main(String[] args) { 
+ArrayList<String> cars = new ArrayList<String>();
+cars.add("Volvo");
+cars.add("BMW");
+cars.add("Ford");
+cars.add("Mazda");
+System.out.println(cars.size());
+} 
+}
