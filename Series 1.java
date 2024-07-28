ad73a0137b20c7cb966fb65f5291f27cd01cd039
Rajasree Laha <laharajasree121@gmail.com>
2024-07-28 10:05:49 +0530

    Create Series 1.java

---
+++ Series 1.java
@@ -0,0 +1,21 @@
+Series 1
+/*1+(1/4)+(1/9)+(1/16)+.....upto n*/
+import java.util.*;
+public class s_1
+{
+ public static void main(String args[])
+ {
+ double sum=0,i,n,a;
+ a=1;
+ System.out.println("enter the value of n");
+ Scanner rl=new Scanner(System.in);
+ n=rl.nextInt();
+ for(i=1;i<=n;i++)
+ {
+ sum=sum+(a/(i*i));
+ }
+ System.out.println("the result of this series is");
+ System.out.println(sum);
+ }
+ 
+}
