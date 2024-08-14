129771719bf487515c7fdceb7cbdc2dd6a9264e7
Rajasree Laha <laharajasree121@gmail.com>
2024-08-14 09:05:55 +0530

    Create smaller values from taken values.java

---
+++ smaller values from taken values.java
@@ -0,0 +1,17 @@
+/*smaller values from taken values*/
+import java.util.Scanner;
+public class smaller_from_taken_value
+{
+ public static void main(String args[])
+ {
+ int n,i;
+ System.out.println("enter a no");
+ Scanner sc=new Scanner(System.in);
+ n=sc.nextInt();
+ System.out.println("the smaller values from the taken value are following");
+ for(i=0;i<n;i++)
+ {
+ System.out.println(i);
+ }
+ }
+}
