06c82e9ab5c9b2ca78d972a77cbd5465af259572
Rajasree Laha <laharajasree121@gmail.com>
2024-10-01 07:34:01 +0530

    Create string copy in another way.java

---
+++ string copy in another way.java
@@ -0,0 +1,15 @@
+/*string copy in another way*/
+import java.util.*;
+public class string_copy_another_way
+{
+ public static void main(String args[])
+ {
+ String x,t;
+ System.out.println("enter your name");
+ Scanner sc=new Scanner(System.in);
+ x=sc.nextLine();
+ t=x;
+ System.out.println("the new string is ");
+ System.out.println(t);
+ }
+}
