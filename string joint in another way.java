6d35d41a9a8b12fa060b47334e2304295d36df6e
Rajasree Laha <laharajasree121@gmail.com>
2024-10-07 09:10:44 +0530

    Create string joint in another way.java

---
+++ string joint in another way.java
@@ -0,0 +1,17 @@
+/*string joint in another way*/
+import java.util.*;
+public class s_joint_other_way
+{
+ public static void main(String args[])
+ {
+ String x,t,k;
+ System.out.println("enter your name");
+ Scanner sc=new Scanner(System.in);
+ x=sc.nextLine();
+ System.out.println("enter your title");
+ t=sc.nextLine();
+ k=x+t;
+ System.out.println("after jointing the new string is");
+ System.out.println(k);
+ }
+}
