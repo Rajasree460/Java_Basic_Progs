0c269504843c227883d2d774056c50532c29e0ca
Rajasree Laha <laharajasree121@gmail.com>
2024-10-08 10:06:24 +0530

    Create string length.java

---
+++ string length.java
@@ -0,0 +1,16 @@
+/*string length*/
+import java.util.*;
+public class string_length
+{
+ public static void main(String args[])
+ {
+ String x;
+ int k;
+ System.out.println("enter your name");
+ Scanner sc=new Scanner(System.in);
+ x=sc.nextLine();
+ k=x.length();
+ System.out.println("the length of this string is");
+ System.out.println(k);
+ }
+}
