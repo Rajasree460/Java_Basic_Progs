8c35eed07246258bc187ef3ffeb5812261ba71e9
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:24:03 +0530

    Create sum using method.java

---
+++ sum using method.java
@@ -0,0 +1,15 @@
+/*sum using method*/ 
+class sum123 
+{ 
+} 
+void sum(int a,int b) 
+{ 
+} 
+int c; 
+c=a+b; 
+System.out.println("the result of the add is: "+c); 
+void main(int a,int b) 
+{ 
+} 
+sum123 sc=new sum123(); 
+sc.sum(a,b); 
