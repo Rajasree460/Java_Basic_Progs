726fadd7f1fe714ff8d5095d9f6eeecbeb3597ee
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:05:29 +0530

    Create string replace.java

---
+++ string replace.java
@@ -0,0 +1,15 @@
+/*string replace*/ 
+import java.util.*; 
+public class string_replace_ 
+{ 
+} 
+public static void main(String args[]) 
+{ 
+} 
+String x,k; 
+System.out.println("enter a string"); 
+Scanner sc=new Scanner(System.in); 
+x=sc.nextLine(); 
+k=x.replaceAll("raj","RAJ"); 
+System.out.println("the new string is"); 
+System.out.println(k); 
