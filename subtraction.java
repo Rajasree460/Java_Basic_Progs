b0ec244d523ed6dfeb9ce5ede1d9eb237353b9aa
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:16:41 +0530

    Create subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,16 @@
+/*subtraction*/ 
+import java.util.Scanner; 
+public class subtraction 
+{ 
+} 
+public static void main(String args[]) 
+{ 
+} 
+int x,y,z; 
+System.out.println("enter 2 no "); 
+Scanner sc=new Scanner(System.in); 
+x=sc.nextInt(); 
+y=sc.nextInt(); 
+z=x-y; 
+System.out.println("the subtraction is"); 
+System.out.println(z); 
