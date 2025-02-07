0bcf614fc18d5c2f828649a7c0f087da2f1c70ea
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:14:12 +0530

    Create string user defined.java

---
+++ string user defined.java
@@ -0,0 +1,16 @@
+/*string user defined*/ 
+import java.util.*; 
+public class string 
+{ 
+} 
+public static void main(String args[]) 
+{ 
+} 
+String x; 
+System.out.println("enter the string"); 
+Scanner sc=new Scanner(System.in); 
+x=sc.nextLine(); 
+System.out.println("the char present in the 2th index of x"); 
+System.out.println(x.charAt(2)); 
+System.out.println("the given string is"); 
+System.out.println(x); 
