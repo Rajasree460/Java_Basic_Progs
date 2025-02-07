f2136f33d18a09e33814d0316183cc51b2d7562a
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:32:10 +0530

    Create type custing.java

---
+++ type custing.java
@@ -0,0 +1,20 @@
+/*type custing*/ 
+import java.util.Scanner; 
+public class type_custing 
+{ 
+    public static void main(String args[]) 
+    { 
+        int x,y; 
+        float z; 
+        System.out.println("enter 2 no"); 
+        Scanner sc=new Scanner(System.in); 
+        x=sc.nextInt(); 
+        y=sc.nextInt(); 
+        System.out.println("the division is"); 
+        z=(float)x/(float)y; 
+        System.out.println(z); 
+         
+         
+         
+    } 
+} 
