789cbeb05d36b4a59386b67081738a0110eb738a
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:09:21 +0530

    Create string upper case.java

---
+++ string upper case.java
@@ -0,0 +1,14 @@
+/*string upper case*/ 
+import java.lang.*; 
+import java.util.*; 
+public class upper_case_string 
+{ 
+} 
+public static void main(String args[]) 
+{ 
+} 
+String x; 
+System.out.println("enter your name"); 
+Scanner sc=new Scanner(System.in); 
+x=sc.nextLine(); 
+System.out.println("after upper case the new string is:  "+x.toUpperCase()); 
