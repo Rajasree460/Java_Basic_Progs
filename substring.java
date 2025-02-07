af720d1f6924d943da2b0f3f9fcaa72b54313857
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:16:10 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,15 @@
+/*substring*/ 
+import java.util.*; 
+public class substring 
+{ 
+} 
+public static void main(String args[]) 
+{ 
+} 
+String x,k; 
+System.out.println("enter a string"); 
+Scanner sc=new Scanner(System.in); 
+x=sc.nextLine(); 
+k=x.substring(1,4); 
+System.out.println("the separated part is"); 
+System.out.println(k); 
