495e709b48a5e571b1fd52ba5a5380a5e04b1c19
Rajasree Laha <laharajasree121@gmail.com>
2025-02-07 22:23:37 +0530

    Create sum of array by using method another.java

---
+++ sum of array by using method another.java
@@ -0,0 +1,30 @@
+/*sum of array by using method*/ 
+import java.util.*; 
+public class sum_array_by_method 
+{ 
+    public static void main(String args[]) 
+    { 
+        int i,n; 
+        System.out.println("enter the value of n"); 
+        Scanner sc=new Scanner(System.in); 
+        n=sc.nextInt(); 
+        int a[]=new int[n]; 
+        System.out.println("enter the values of array"); 
+        for(i=0;i<n;i++) 
+        { 
+            a[i]=sc.nextInt(); 
+        } 
+        sum(a,n); 
+    } 
+    public static void sum(int a1[],int n1) 
+    { 
+        int i,sum=0; 
+        for(i=0;i<n1;i++) 
+        { 
+            sum=sum+a1[i];  
+        } 
+        System.out.println("the sum is"); 
+        System.out.println(sum); 
+ 
+    } 
+} 
